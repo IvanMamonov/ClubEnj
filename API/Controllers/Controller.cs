fe48ca8490f25e71e6f4c1175b07e4dd65913d51
Иван Мамонов <mamonov_vanya03@mail.ru>
2022-05-30 06:08:51 +0300

    add console

--- API/Controllers/Controller.cs
+++ API/Controllers/Controller.cs
@@ -11,55 +11,6 @@ namespace API.Controllers
     [Route("[controller]")]
     public class Controller : ControllerBase
     {
-        // GET: api/<ClientController>
-        [HttpGet]
-        public IEnumerable<Problems> Get()
-        {
-            return DataManag.GetProblems();
-        }
-
-        // GET api/<ClientController>/5
-        [HttpGet("{id}")]
-        public ActionResult<Problems> Get(int id)
-        {
-            var result = DataManag.GetProblem(id);
-            if (result == null)
-                return NotFound();
-
-            return result;
-        }
-
-        // POST api/<ClientController>
-        [HttpPost]
-        public IActionResult Create(Problems problems)
-        {
-            DataManag.AddProblem(problems);
-            return Content("ADDED");
-        }
-
-        // PUT api/<ClientController>/5
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Problems problems)
-        {
-            var result = DataManag.GetProblem(id);
-            if (result == null)
-                return NotFound();
-
-            DataManag.UpdateProblems(id, problems);
-
-            return Content("UPDATED");
-        }
-
-        // DELETE api/<ClientController>/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var result = DataManag.GetProblem(id);
-            if (result == null)
-                return NotFound();
-
-            DataManag.RemoveProblem(id);
-            return Content("DELETED");
-        }
+        
     }
 }
---
+++ Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using club;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static User user;
+        private ObservableCollection<User> users = new ObservableCollection<User>(bd_connection.connection.User);
+        static void Main(string[] args)
+        {
+            bool AddUser = false;
+            while (!AddUser)
+            {
+                Console.WriteLine("[+] Введите имя:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("[+] Введите номер телефона:");
+                string userNumber = Console.ReadLine();
+            }
+            if (userName != null && userNumber != null)
+            {
+                User user = new User();
+            }
+        }
+    }
+}
